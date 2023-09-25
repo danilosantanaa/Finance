cab650bb1c84505b4648ee5f7522d7870a33f43f
Danilo Santana <38994152+danilosantanaa@users.noreply.github.com>
2023-09-25 17:57:13 -0300

    Implementação do servicos de upload

---
+++ Back/src/Finance.Application/Contratos/IUploadService.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Finance.Application.Contratos
+{
+    public interface IUploadService
+    {
+        bool Delete(string fileName, string directory_path = "");
+        bool Delete(string[] filesName, string directory_path = "");
+        Task<string> Save(IFormFile file, string directory_path = "");
+        Task<string[]> Save(IFormFile[] file, string directory_path = "");
+    }
+}
